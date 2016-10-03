c0681874c7d91f4e419fbf3ca90a1584ea74fa7c
Feifei GU <gufeifei@outlook.com>
2016-10-03 21:33:42 +0800

    adding two models

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
