3163b37534a4cee1a4bf26ccec2853dfb01b3272
Feifei GU <gufeifei@outlook.com>
2016-10-03 22:00:57 +0800

    adding a controller and action named Create

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            return View(); // this view is under views/gigs/create
+        }
+    }
+}
