09128f19cde3d0940dbc1cc10773234ee5d5da9f
Feifei GU <gufeifei@outlook.com>
2016-10-04 15:03:42 +0800

    PM> add-migration AddForeignKeyPropertiesToGig

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,22 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+using System;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public GigsController()
         {
             _context = new ApplicationDbContext();
         }
-            // GET: Gigs
+
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -25,6 +26,28 @@ namespace GigHub.Controllers
             };
 
             return View(viewModel); // this view is under views/gigs/create
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            //get the application user object
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0},{1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("index", "Home");
         }
     }
 }
--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -11,8 +11,11 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
@@ -20,7 +23,9 @@ namespace GigHub.Models
         [StringLength(255)]
         public string Venue { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
     }
 }
