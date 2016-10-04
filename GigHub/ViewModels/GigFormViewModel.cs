318ecceea67df6803f9ac6bbf370a33f6dabf4b1
Feifei GU <gufeifei@outlook.com>
2016-10-04 15:44:23 +0800

    adding default validation

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using GigHub.Models;
@@ -8,13 +9,22 @@ namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
         public string Date { get; set; }
+
+        [Required]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime {
-            get { return DateTime.Parse(string.Format("{0},{1}", Date, Time)); }
+
+        public DateTime GetDateTime() {
+            return DateTime.Parse(string.Format("{0}{1}",Date,Time));
         }
     }
 }
