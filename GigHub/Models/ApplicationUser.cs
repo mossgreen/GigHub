0cef0b275e81e29f01998cded121c528fff179be
Feifei GU <gufeifei@outlook.com>
2016-10-08 21:10:16 +0800

    PM> add-migration AddFollowing -force
    adding model following

--- GigHub/Models/ApplicationUser.cs
+++ GigHub/Models/ApplicationUser.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -12,6 +15,18 @@ namespace GigHub.Models
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
+
+        public ICollection<Following> Followers{ get; set; }
+        public ICollection<Following> Followees { get; set; }
+
+        /*when you add a property to a class, and property is a collection,
+         you should always initialize it in the constructor,
+         It's the responsibility of this class*/
+        public ApplicationUser()
+        {
+            Followers = new Collection<Following>();
+            Followees = new Collection<Following>();
+        }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
