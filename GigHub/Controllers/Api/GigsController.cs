e8b0d8b5455383475f3c5c05391c6b8fcab16906
Feifei GU <gufeifei@outlook.com>
2016-10-20 20:55:44 +0800

    add GigsController and ajax method

---
+++ GigHub/Controllers/Api/GigsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers.Api
+{
+
+    [Authorize]
+    public class GigsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            gig.IsCanceled = true;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
