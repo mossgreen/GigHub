fe250944adec35780827f74b4514c554549c9729
Feifei GU <gufeifei@outlook.com>
2016-10-29 23:33:06 +0800

    Using Camel Notation

--- GigHub/App_Start/WebApiConfig.cs
+++ GigHub/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace GigHub
 {
@@ -9,6 +11,11 @@ namespace GigHub
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings;
+
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
