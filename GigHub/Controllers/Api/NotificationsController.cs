77b3064c49d742a208627c3059354933bcf347d4
Feifei GU <gufeifei@outlook.com>
2016-10-29 23:39:07 +0800

    bug fixed

--- GigHub/Controllers/Api/NotificationsController.cs
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using AutoMapper;
+using GigHub.Dtos;
 using GigHub.Models;
 using Microsoft.AspNet.Identity;
 
@@ -24,7 +25,7 @@ namespace GigHub.Controllers.Api
         {
             var userId = User.Identity.GetUserId();
             var notifications = _context.UserNotifications
-                .Where(un => un.UserId == userId)
+                .Where(un => un.UserId == userId && !un.IsRead)
                 .Select(un => un.Notification)
                 .Include(n => n.Gig.Artist)
                 .ToList();
