d186524acdc8f334fe5cdd2140b9dafe8ed13e77
Feifei GU <gufeifei@outlook.com>
2016-11-11 23:08:46 +0800

    add details page

---
+++ GigHub/Controllers/GigDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GigHub.Models;
+
+namespace GigHub.Controllers
+{
+    public class GigDetailsViewModel
+    {
+        public Gig Gig { get; set; }
+        public bool IsAttending { get; set; }
+        public bool IsFollowing { get; set; }
+
+    }
+}
