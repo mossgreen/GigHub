ef836799603a847f1b05b021800fb6a757967b4f
Feifei GU <gufeifei@outlook.com>
2017-02-04 13:43:21 +0800

    extracting interfaces

---
+++ GigHub/Repositories/IGigRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GigHub.Models;
+
+namespace GigHub.Repositories
+{
+    public interface IGigRepository
+    {
+        Gig GetGig(int id);
+        IEnumerable<Gig> GetGigsUserAttending(string userId);
+        Gig GetGigWithAttendees(int gigId);
+        IEnumerable<Gig> GetUpcomingGigsByArtist(string userId);
+        void Add(Gig gig);
+    }
+}
